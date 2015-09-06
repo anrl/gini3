99c92a6e491bb17205b615b3dd8979895e3aa5bd
anrl <anrl@lab>
2015-09-06 17:40:43 -0400

    Bug fixes

--- backend/src/grouter/vpl.c
+++ backend/src/grouter/vpl.c
@@ -93,7 +93,7 @@ vpl_data_t *vpl_connect(char *vsock_name)
 	} name;
 
 	verbose(2, "[vpl_connect]:: starting connection.. ");
-	vpl_data_t *pri = (vpl_data_t *)calloc(sizeof(vpl_data_t));
+	vpl_data_t *pri = (vpl_data_t *)calloc(1, sizeof(vpl_data_t));
 	pri->sock_type = "unix";
 	pri->ctl_sock = strdup(vsock_name);
 	pri->ctl_addr = new_addr(pri->ctl_sock,
