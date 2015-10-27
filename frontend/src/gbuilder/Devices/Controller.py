8f79e4b48fc7838fdde3947bd828e18f5e6d4200
Michael Williams <draringi@draringi.net>
2015-10-26 22:01:17 -0400

    Initial Controller UI Object

---
+++ frontend/src/gbuilder/Devices/Controller.py
@@ -0,0 +1,22 @@
+from Core.Device import *
+
+class Switch(Device):
+    device_type = "Openflow Controller"
+    
+    def __init__(self):
+        Device.__init__(self)
+
+    def addEdge(self, edge):
+        Device.addEdge(self, edge)
+
+        node = edge.getOtherDevice(self)
+        if node.device_type == "Router":
+            node.addInterface(self)
+
+    def removeEdge(self, edge):
+        Device.removeEdge(self, edge)
+
+        node = edge.getOtherDevice(self)
+        if node.device_type == "Router":
+            node.removeInterface(self)
+
