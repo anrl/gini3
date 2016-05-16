b5a54fc8486477c1bce1e4f535bbd8dd6c85f9e5
akankshakmr174 <akankshakmr174@gmail.com>
2016-05-16 09:16:41 -0400

    Updated UI and removed unimplemented types

--- frontend/src/gbuilder/Core/Item.py
+++ frontend/src/gbuilder/Core/Item.py
@@ -6,14 +6,14 @@ from PyQt4 import QtCore
 realMnumber=2
 alist={'m1ip':'1','m1name':'1','m1mac':'1','m1port':'1','m2ip':'2','m2name':'2','m2mac':'2','m2port':'2'}
 # The list of device types and their current index numbers
-hostTypes = {'REALM':0, "UML":0, "UML_FreeDOS":0, "UML_Android":0, "Mobile":0}
-netTypes = {"Switch":0, "Subnet":0, "Router":0, "Wireless_access_point":0, "Firewall":0, "OpenFlow_Controller":0}
+hostTypes = {'REALM':0, "UML":0, "Mobile":0}
+netTypes = {"Switch":0, "Subnet":0, "Router":0, "Wireless_access_point":0, "OpenFlow_Controller":0}
 customTypes = {"Custom":0}
-nodeTypes = {"REALM":hostTypes, "UML":hostTypes, "UML_FreeDOS":hostTypes, "UML_Android":hostTypes, "Mobile":hostTypes, "Switch":netTypes, "Subnet":netTypes, "Router":netTypes,
-             "Wireless_access_point":netTypes, "Firewall":netTypes, "OpenFlow_Controller":netTypes, "Custom":customTypes}
+nodeTypes = {"REALM":hostTypes, "UML":hostTypes, "Mobile":hostTypes, "Switch":netTypes, "Subnet":netTypes, "Router":netTypes,
+             "Wireless_access_point":netTypes, "OpenFlow_Controller":netTypes, "Custom":customTypes}
 
 commonTypes = ["UML", "Subnet", "Switch", "Router"]
-unimplementedTypes = ["UML_FreeDOS", "UML_Android", "Firewall"]
+unimplementedTypes = ["UML_FreeDOS", "UML_Android", "Firewall"] #removed the unimplementedTypes from the UI
 
 class Item(object):
     def getName(self):
