8ffd11bca637afb3e6c8d6238ea0b7d0734cad73
Muthucumaru Maheswaran <mahes25@gmail.com>
2017-06-03 21:19:01 -0400

    added missing device defs

--- backend/include/devicedefs.h
+++ backend/include/devicedefs.h
@@ -10,16 +10,16 @@
 
 
 // These are device names... not all are defined!
-#define ETHERNET_DEVICE		    	"eth"
-#define TAP_DEVICE					"tap"
-#define TUN_DEVICE              	"tun"
-#define WIRELESS_LAN_DEVICE		"wlan"
-#define PPP_DEVICE					"ppp"
-#define TOKEN_RING_DEVICE			"tr"
-#define AMATEUR_RADIO_DEVICE	   "ax"
-#define SLIP_DEVICE					"sl"
-#define PARALLEL_LINE_DEVICE		"plip"
-
+#define ETHERNET_DEVICE		"eth"
+#define TAP_DEVICE		"tap"
+#define TUN_DEVICE              "tun"
+#define WIRELESS_LAN_DEVICE	"wlan"
+#define PPP_DEVICE		"ppp"
+#define TOKEN_RING_DEVICE	"tr"
+#define AMATEUR_RADIO_DEVICE	"ax"
+#define SLIP_DEVICE		"sl"
+#define PARALLEL_LINE_DEVICE	"plip"
+#define RAW_DEVICE              "raw"
 
 #define LIST_OF_DEVICES   devicedirectory_t devdir[] = { \
 	{  \
@@ -34,11 +34,17 @@
 		fromTapDev, \
 		toTapDev, \
 	}, \
-	{ \
+        { \
 		TUN_DEVICE, \
 		"TUNNEL DEVICE DRIVER", \
 		fromTunDev, \
 		toTunDev, \
+	}, \
+        { \
+		RAW_DEVICE, \
+		"RAW DEVICE DRIVER", \
+		fromRawDev, \
+		toRawDev, \
 	} \
 }
 
